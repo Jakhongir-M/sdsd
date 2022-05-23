0ab632517600ccc8a61fe631d3b4b62278446044
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-23 18:06:50 +0500

    firstcommit

---
+++ app.js
@@ -0,0 +1,27 @@
+let days = +prompt("Напиши тут от 1 до 7 цифр и я буду называть тебя так");
+switch (days) {
+  case 1:
+    document.write(" Мандаринка 😍");
+    break;
+  case 2:
+    document.write(" моя шоколадка 💋");
+    break;
+  case 3:
+    document.write(" моя любимая 🥰");
+    break;
+  case 4:
+    document.write(" мой снежок 😘");
+    break;
+  case 5:
+    document.write("мой бриллиант 🤯");
+    break;
+  case 6:
+    document.write(" мой снежок 🥶");
+    break;
+  case 7:
+    document.write("МАНДАРИНКА  😜");
+    break;
+  default:
+    alert("ты не ту цифру вводила 😢");
+    break;
+}
